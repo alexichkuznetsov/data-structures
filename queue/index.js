253a22ec31a29720df9da2f3c3c064fdae642251
Aleksey <alexichkuznetsov@gmail.com>
2019-06-04 19:05:55 +0300

    Added queue

---
+++ queue/index.js
@@ -0,0 +1,73 @@
+/**
+ * Class to create a Node object
+ */
+class Node {
+  /**
+   * Node constructor
+   * @param {any} value
+   * @return {Object}
+   */
+  constructor(value) {
+    this.next = null;
+    this.value = value;
+  }
+}
+
+/**
+ * Class to create a Queue object
+ */
+class Queue {
+  /**
+   * Queue constructor
+   */
+  constructor() {
+    this.start = null;
+    this.end = null;
+  }
+
+  /**
+   * Add new element to the queue
+   * @param {any} value
+   * @return {Object}
+   */
+  enqueue(value) {
+    const newNode = new Node(value);
+
+    if (!this.start) {
+      this.start = newNode;
+      this.end = newNode;
+
+      return this;
+    }
+
+    const prevEnd = this.end;
+
+    prevEnd.next = newNode;
+    this.end = newNode;
+
+    return this;
+  }
+
+  /**
+   * Remove an element from the queue
+   * @return {Object | undefined}
+   */
+  dequeue() {
+    const removedNode = this.start;
+
+    if (!removedNode) return undefined;
+
+    if (this.start === this.end) {
+      this.start = null;
+      this.end = null;
+
+      return removedNode;
+    }
+
+    const newStart = removedNode.next;
+    this.start = newStart;
+    removedNode.next = null;
+
+    return removedNode;
+  }
+}
